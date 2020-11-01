10f609afa54b0d192e9a7a74fa23ca1513d7bde7
Viktor Tishchenko <web22pro@gmail.com>
2020-11-02 01:44:43 +0600

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,58 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct) {
+    this.direct = direct;
+  }
+
+  encrypt(str, key) {
+    if (!str || !key) throw new Error();
+
+    str = str.toUpperCase();
+    if (this.direct === false) str = str.split('').reverse().join('');
+    key = key.toUpperCase();
+    while (key.length < str.length) key += key;
+    let res = [];
+    let idx = 0;
+
+    for (let i = 0; i < str.length; i++) {
+      let charCode = str.charCodeAt(i);
+
+      if (charCode >= 65 && charCode <= 90) {
+        let charKey = key.charCodeAt(idx++ % key.length);
+        let charEncrypt = charCode + charKey - 65;
+
+        if (charEncrypt > 90) charEncrypt -= 26;
+        res.push(String.fromCharCode(charEncrypt));
+      } else {
+        res.push(str[i]);
+      }
+    }
+    return res.join('');
+  }
+
+  decrypt(str, key) {
+    if (!str || !key) throw new Error();
+
+    str = str.toUpperCase();
+    if (this.direct === false) str = str.split('').reverse().join('');
+    key = key.toUpperCase();
+    while (key.length < str.length) key += key;
+    let res = [];
+    let idx = 0;
+
+    for (let i = 0; i < str.length; i++) {
+      let charCode = str.charCodeAt(i);
+
+      if (charCode >= 65 && charCode <= 90) {
+        let charCodeKey = key.charCodeAt(idx++ % key.length);
+        let charDecrypt = charCode - charCodeKey + 65;
+
+        if (charDecrypt < 65) charDecrypt += 26;
+        res.push(String.fromCharCode(charDecrypt));
+      } else {
+        res.push(str[i]);
+      }
+    }
+    return res.join('');
   }
 }
 
